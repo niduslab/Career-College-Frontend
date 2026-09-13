cb78edaf1d0eaf9913bee1ce090c7f7e7bfc1c4d
alamincse2003 <mdalamincse2003@gmail.com>
2026-09-13 08:41:01 +0600

    feat: comment out PlatformFeatures and FavoriteMentors components in HomePage

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -21,10 +21,10 @@ export default function HomePage() {
       <PopularCourses />
       <CareerJourney />
       <TrendingCourses />
-      <PlatformFeatures />
+      {/* <PlatformFeatures /> */}
       <LearningJourneySteps />
       <InstructorsSection />
-      <FavoriteMentors />
+      {/* <FavoriteMentors /> */}
       <Testimonials />
       <UpcomingWebinars />
       <FaqSection />
